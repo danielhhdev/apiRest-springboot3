3ea86e0dfcc4b0357fbdb58a73b17a5be6f6c746
HERNANDEZ HUERTA Daniel <daniel.hernandez@soprasteria.com>
2025-06-02 09:55:25 +0200

    primer commit

---
+++ src/main/java/com/dhh/apiRestSpringboot3/ApiRestSpringboot3Application.java
@@ -0,0 +1,13 @@
+package com.dhh.apiRestSpringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringboot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhh/apiRestSpringboot3/ApiRestSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhh.apiRestSpringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
