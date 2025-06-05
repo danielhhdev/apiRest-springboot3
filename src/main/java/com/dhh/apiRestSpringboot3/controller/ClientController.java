57174e71e25d9edc0ebb12952e65b490170e6c93
Daniel Hernández <daniel.hhuerta@gmail.com>
2025-06-05 11:29:21 +0200

    cambio para graavl

--- src/main/java/com/dhh/apiRestSpringboot3/controller/ClientController.java
+++ src/main/java/com/dhh/apiRestSpringboot3/controller/ClientController.java
@@ -3,9 +3,12 @@ package com.dhh.apiRestSpringboot3.controller;
 import com.dhh.apiRestSpringboot3.dto.ClientDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -33,8 +36,8 @@ public interface ClientController {
             @ApiResponse(responseCode = "500", description = "Error interno del servidor")
     })
     ResponseEntity<ClientDTO> create(
-            @Parameter(description = "DTO del cliente a crear", required = true) ClientDTO clienteDTO
-    );
+            @Parameter(description = "DTO del cliente a crear", required = true) @Valid @RequestBody ClientDTO clienteDTO
+            );
 
     @Operation(
             summary = "Actualizar un cliente",
@@ -47,8 +50,8 @@ public interface ClientController {
             @ApiResponse(responseCode = "500", description = "Error interno del servidor")
     })
     ResponseEntity<ClientDTO> update(
-            @Parameter(description = "ID del cliente a actualizar", required = true) Long id,
-            @Parameter(description = "DTO del cliente con los nuevos datos", required = true) ClientDTO clienteDTO
+            @Parameter(description = "ID del cliente a actualizar", required = true) @PathVariable Long id,
+            @Parameter(description = "DTO del cliente con los nuevos datos", required = true) @Valid @RequestBody ClientDTO clienteDTO
     );
 
     @Operation(
@@ -61,6 +64,6 @@ public interface ClientController {
             @ApiResponse(responseCode = "500", description = "Error interno del servidor")
     })
     ResponseEntity<Void> delete(
-            @Parameter(description = "ID del cliente a eliminar", required = true) Long id
+            @Parameter(description = "ID del cliente a eliminar", required = true) @PathVariable Long id
     );
 }
