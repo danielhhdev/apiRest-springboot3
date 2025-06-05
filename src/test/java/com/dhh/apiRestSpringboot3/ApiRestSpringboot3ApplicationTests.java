1a62e8ee13ff81c6ce1f8f2e3e0b39baf1d7da7d
Daniel Hernández <daniel.hhuerta@gmail.com>
2025-06-05 13:07:25 +0200

    modificacion test integracion

--- src/test/java/com/dhh/apiRestSpringboot3/ApiRestSpringboot3ApplicationTests.java
+++ src/test/java/com/dhh/apiRestSpringboot3/ApiRestSpringboot3ApplicationTests.java
@@ -1,10 +1,11 @@
 package com.dhh.apiRestSpringboot3;
 
+import com.dhh.apiRestSpringboot3.integracion.BaseIntegracionTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiRestSpringboot3ApplicationTests {
+class ApiRestSpringboot3ApplicationTests extends BaseIntegracionTest {
 
 	@Test
 	void contextLoads() {
