a91acaa752d3714476ef56c94a7d31aea3246358
Daniel Hernández <daniel.hhuerta@gmail.com>
2025-06-07 08:01:38 +0200

    cambios DB 2

--- src/test/java/com/dhh/apiRestSpringboot3/ApiRestSpringboot3ApplicationTests.java
+++ src/test/java/com/dhh/apiRestSpringboot3/ApiRestSpringboot3ApplicationTests.java
@@ -4,12 +4,14 @@ import com.dhh.apiRestSpringboot3.integracion.BaseIntegracionTest;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class ApiRestSpringboot3ApplicationTests{
+@ActiveProfiles("test")
+class ApiRestSpringboot3ApplicationTests extends BaseIntegracionTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
